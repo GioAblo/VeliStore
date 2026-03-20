784af2af1444c8a8dc9bbb0cef3a38d48d6007ff
Giorgi Ablotia <ablootia1993@gmail.com>
2026-03-21 00:24:14 +0400

    fro deploy 2

--- src/services/api/allUrl.ts
+++ src/services/api/allUrl.ts
@@ -1,23 +1,23 @@
 // Auth
-export const API_SIGNIN_URL = "http://localhost:3000/auth/login"
+export const API_SIGNIN_URL = "https://backend-veli-store-6.onrender.com/auth/login"
 export const API_SIGNUP_URL = "http://localhost:3000/auth/register"
 
 // User
-export const API_GET_CURRENT_USER_URL = "http://localhost:3000/user/current-user"
-export const API_UPDATE_CURRENT_USER_URL = "http://localhost:3000/user"
+export const API_GET_CURRENT_USER_URL = "https://backend-veli-store-6.onrender.com/user/current-user"
+export const API_UPDATE_CURRENT_USER_URL = "https://backend-veli-store-6.onrender.com/user"
 
 // Products
-export const API_GET_PRODUCTS_URL = "http://localhost:3000/product?pageSize=300"
-export const API_GET_PRODUCTS_FILTERS_URL = "http://localhost:3000/product"
+export const API_GET_PRODUCTS_URL = "https://backend-veli-store-6.onrender.com/product?pageSize=300"
+export const API_GET_PRODUCTS_FILTERS_URL = "https://backend-veli-store-6.onrender.com/product"
 
 // Category
-export const API_GET_CATEGORY_URL = "http://localhost:3000/product-category"
+export const API_GET_CATEGORY_URL = "https://backend-veli-store-6.onrender.com/product-category"
 
 // cart
-export const API_GET_CART_URL = "http://localhost:3000/cart"
+export const API_GET_CART_URL = "https://backend-veli-store-6.onrender.com/cart"
 
 //wishlist 
-export const API_WISHLIST_URL = "http://localhost:3000/liked-products"
+export const API_WISHLIST_URL = "https://backend-veli-store-6.onrender.com/liked-products"
 
 // Purchases 
-export const API_PURCHASES_URL = "http://localhost:3000/purchases"
+export const API_PURCHASES_URL = "https://backend-veli-store-6.onrender.com/purchases"
